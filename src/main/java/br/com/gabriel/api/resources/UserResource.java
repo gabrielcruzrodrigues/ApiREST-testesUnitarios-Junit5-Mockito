42a344680f62929a34fd5eaefd8a52d030c0ee11
Gabriel <contatogabrielcruzrodrigues@gmail.com>
2023-09-21 02:31:39 -0300

    User resource FindById

--- src/main/java/br/com/gabriel/api/resources/UserResource.java
+++ src/main/java/br/com/gabriel/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.gabriel.api.resources;
 
 import br.com.gabriel.api.domain.User;
+import br.com.gabriel.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(new User(1, "gabriel", "gabriel@", "123"));
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 
 }
